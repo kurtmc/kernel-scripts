4a9146482f557c4007e881f44f884e4fe1175b1e
Kurt McAlpine <kurt@linux.com>
2015-04-15 16:59:15 +1200

    Created a script to compile and install kernels for syslinux

---
+++ compile_and_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Read KERNELRELEASE from include/config/kernel.release (if it exists) (This line is stolen from the linux kernel Makefile)
+KERNELRELEASE=$(cat include/config/kernel.release 2> /dev/null)
+
+# compile kernel
+#make -j4
+
+# install modules
+#sudo make modules_install
+
+# copy image to /boot
+cp -v arch/x86/boot/bzImage /boot/vmlinuz_$KERNELRELEASE
+
+# make initial RAM disk
+mkinitcpio -k $KERNELRELEASE -c /etc/mkinitcpio.conf -g /boot/initramfs_$KERNELRELEASE.img
+
+# update /boot/syslinux/syslinux.cfg
+echo "
+LABEL $KERNELRELEASE
+	MENU LABEL $KERNELRELEASE
+	LINUX ../vmlinuz_$KERNELRELEASE
+	APPEND root=/dev/sda1 rw
+	INITRD ../initramfs_$KERNELRELEASE.img" >> /boot/syslinux/syslinux.cfg
+
+
+
+
+
+
+
+
+
